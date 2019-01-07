c87b95cb95c6853ac6c452cb0b6f5d17e5d146cd
Matthew Green <mgreen27@gmail.com>
2019-01-07 14:23:10 +1100

    Powershell-IR/Fix sbRecycle scriptblock

--- Scriptblock/sbRecycle.ps1
+++ Scriptblock/sbRecycle.ps1
@@ -5,5 +5,7 @@ Write-Host -ForegroundColor Yellow "`tCollecting RecycleBin Artefacts"
 $Drives = [System.IO.DriveInfo]::getdrives() | Where-Object {$_.DriveType -eq 'Fixed'} | Select-Object -Property Name
 
 foreach ( $Drive in $Drives ) {
-    Invoke-BulkCopy -folder "$Drive\`$Recycle.Bin" -target "$Output\RecycleBin\$($Drives.Name.trimend("\").trimend(":"))" -recurse -Forensic -Exclude desktop.ini
+    $target = $Drive.Name.trimend(":\")
+
+    Invoke-BulkCopy -folder "$Drive\`$Recycle.Bin" -target "$Output\RecycleBin\$Target" -recurse -Forensic -Exclude desktop.ini
 }
