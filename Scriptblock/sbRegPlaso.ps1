e0e9ec8013d050fd9c111dab0d50e678b0f41bec
Matthew Green <mgreen27@gmail.com>
2019-01-09 15:20:59 +1100

    Invoke-IR\Registry update

--- Scriptblock/sbRegPlaso.ps1
+++ Scriptblock/sbRegPlaso.ps1
@@ -1,20 +1,17 @@
 ﻿
-# Windows System Registry hives
+# Windows System Registry hives supported by plaso
 Write-Host -ForegroundColor Yellow "`tCollecting Windows System Registry hives"
 
-Invoke-BulkCopy -folder "$env:systemdrive\Windows\System32\config" -target "$Output\Registry" -filter "SECURITY*" -forensic
-Invoke-BulkCopy -folder "$env:systemdrive\Windows\System32\config" -target "$Output\Registry" -filter "SOFTWARE*" -forensic
-Invoke-BulkCopy -folder "$env:systemdrive\Windows\System32\config" -target "$Output\Registry" -filter "SAM*" -forensic
-Invoke-BulkCopy -folder "$env:systemdrive\Windows\System32\config" -target "$Output\Registry" -filter "SYSTEM*" -forensic
+# relevent hives and recursive folders including ..\regback
+Invoke-BulkCopy -folder "$env:systemdrive\Windows\System32\config" -target "$Output\Registry" -filter "SECURITY*" -forensic -recurse
+Invoke-BulkCopy -folder "$env:systemdrive\Windows\System32\config" -target "$Output\Registry" -filter "SOFTWARE*" -forensic -recurse
+Invoke-BulkCopy -folder "$env:systemdrive\Windows\System32\config" -target "$Output\Registry" -filter "SAM*" -forensic -recurse
+Invoke-BulkCopy -folder "$env:systemdrive\Windows\System32\config" -target "$Output\Registry" -filter "SYSTEM*" -forensic -recurse
 
-Invoke-BulkCopy -folder "$env:systemdrive\Windows\System32\config" -target "$Output\Registry\RegBack" -filter "SECURITY" -forensic
-Invoke-BulkCopy -folder "$env:systemdrive\Windows\System32\config" -target "$Output\Registry\RegBack" -filter "SOFTWARE" -forensic
-Invoke-BulkCopy -folder "$env:systemdrive\Windows\System32\config" -target "$Output\Registry\RegBack" -filter "SAM" -forensic
-Invoke-BulkCopy -folder "$env:systemdrive\Windows\System32\config" -target "$Output\Registry\RegBack" -filter "SYSTEM" -forensic
-Invoke-BulkCopy -folder "$env:systemdrive\Windows\System32\config" -target "$Output\Registry\RegBack" -filter "*.LOG*" -forensic
+# System ntuser.dat and log files
+Invoke-BulkCopy -folder "$env:systemdrive\Windows\System32\config" -target "$Output\Registry" -filter "*.dat" -forensic -recurse
+Invoke-BulkCopy -folder "$env:systemdrive\Windows\System32\config" -target "$Output\Registry\" -filter "*.LOG*" -forensic -recurse
 
-Invoke-BulkCopy -folder "$env:systemdrive\Windows\system32\config\systemprofile" -target "$Output\Registry\SystemProfile" -filter "ntuser.dat" -forensic
-Invoke-BulkCopy -folder "$env:systemdrive\Windows\system32\config\systemprofile" -target "$Output\Registry\SystemProfile" -filter "ntuser.dat.LOG*" -forensic
-
+# will collect service profile user hive
 Invoke-BulkCopy -folder "$env:systemdrive\Windows\ServiceProfiles" -target "$Output\Registry\ServiceProfiles" -filter "ntuser.dat" -recurse -forensic
 Invoke-BulkCopy -folder "$env:systemdrive\Windows\ServiceProfiles" -target "$Output\Registry\ServiceProfiles" -filter "ntuser.dat.LOG*" -recurse -forensic
