195154456868bf9d527e246b63103b56454928c0
mgreen27 <>
2019-01-12 21:30:43 +1100

    ancilaries folder restructure

---
+++ Content/Ancilaries/Get-Forensicating.ps1
@@ -0,0 +1,99 @@
+﻿<#
+.SYNOPSIS
+    A script to download and install Invoke-LiveResponse in users Powershell profile
+
+    Name: Get-Forensicating.ps1
+    Version: 1.2
+
+.DESCRIPTION
+    Remove old versions of Invoke-LiveResponse if exist.
+    Proxy aware download of the latest version of Invoke-LiveResponse.
+    Install in user profile
+    Show installed modules.
+.EXAMPLE
+    # download
+    $url="https://raw.githubusercontent.com/mgreen27/Powershell-IR/master/Get-Forensicating.ps1"
+    [Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
+    $WebClient=(New-Object System.Net.WebClient)
+    $WebClient.Proxy=[System.Net.WebRequest]::GetSystemWebProxy()
+    $WebClient.Proxy.Credentials=[System.Net.CredentialCache]::DefaultNetworkCredentials
+    Invoke-Expression $WebClient.DownloadString($url) 
+
+    # local
+    Set-ExecutionPolicy -ExecutionPolicy Bypass; .\Get-Forensicating.ps1
+.LINK
+    https://github.com/Invoke-IR/PowerForensics
+    https://github.com/mgreen27/Powershell-IR
+
+.NOTES
+    
+#>
+
+Set-ExecutionPolicy -ExecutionPolicy bypass -Force
+
+function Expand-ZIPFile($file, $destination)
+{
+    $shell = new-object -com shell.application
+    $zip = $shell.NameSpace($file)
+    foreach($item in $zip.items()){
+        $shell.Namespace($destination).copyhere($item)
+    }
+}
+
+$target = $env:PSModulePath.split(";")[0]
+
+#Cover usecase of environment variable set but no folder
+If (!(test-path $target)){New-Item $target -type Directory -Force |Out-Null}
+
+# Remove Invoke-LiveResponse
+Remove-Module Invoke-LiveResponse -ErrorAction SilentlyContinue
+If (Test-Path ($Target + "\Invoke-LiveResponse") -ErrorAction SilentlyContinue) {
+        Remove-Item ($Target + "\Invoke-LiveResponse") -Recurse -Force -ErrorAction Stop
+}
+
+# Download Invoke-LiveResponse
+Try{
+    Write-Host -ForegroundColor Yellow "`nDownloading Invoke-LiveResponse... " -NoNewline
+    if (Test-Path "$Target\master.zip" -ErrorAction SilentlyContinue) {
+        Remove-Item "$Target\master.zip" -Force -ErrorAction SilentlyContinue
+    }
+
+    [Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
+    $WebClient=(New-Object System.Net.WebClient)
+    $WebClient.Proxy=[System.Net.WebRequest]::GetSystemWebProxy()
+    $WebClient.Proxy.Credentials=[System.Net.CredentialCache]::DefaultNetworkCredentials
+    $WebClient.DownloadFile("https://github.com/mgreen27/Powershell-IR/archive/master.zip","$Target\invoke-ir.zip")
+    Write-Host -ForegroundColor Red "DONE"
+}
+Catch{
+    Write-Host -ForegroundColor Red "Download Failed!"
+    Break
+}
+
+# Extract Invoke-Live Response
+Try{
+    Write-Host -ForegroundColor Yellow "`nExtracting Invoke-LiveResponse... " -NoNewline
+    unblock-file -Path "$Target\invoke-ir.zip"
+
+    Expand-ZIPFile –File "$target\invoke-ir.zip" –Destination $target
+    Write-Host -ForegroundColor Red "DONE`n"
+
+    Move-Item ($Target + "\Powershell-IR-master") ($Target + "\Invoke-LiveResponse") -Force
+    Remove-Item -Path "$Target\invoke-ir.zip" -Force
+}
+Catch{
+    Write-Host -ForegroundColor Red "Extraction Failed!"
+    Break
+}
+
+# Import Invoke-LiveResponse
+Try{
+    Write-Host -ForegroundColor Yellow "`nAvailible Invoke-LiveResponse functions:"
+    Import-Module -Name Invoke-LiveResponse -WarningAction SilentlyContinue
+    Get-Command -Module Invoke-LiveResponse
+}
+Catch{
+    Write-Host -ForegroundColor Red "Invoke-LiveResponse Import Failed!`n"
+    Break
+}
+
