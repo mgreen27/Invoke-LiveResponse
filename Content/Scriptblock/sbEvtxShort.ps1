9c218e542e4f558bff4aba4e806fdfd0cc90baa5
mgreen27 <>
2019-04-11 17:33:50 +0800

    Invoke-LiveResponse/add evtx update

---
+++ Content/Scriptblock/sbEvtxShort.ps1
@@ -0,0 +1,16 @@
+﻿
+# Windows EventLog collection
+Write-Host -ForegroundColor Yellow "`tCollecting Windows Event Logs"
+$In = "$env:systemdrive\Windows\System32\winevt\Logs"
+$Out = "$Output\" + $env:systemdrive.TrimEnd(':') + "\Windows\System32\winevt\Logs"
+
+# Basic triage, see sbEvtxFull for comprehensive
+Copy-LiveResponse -path $In -dest $Out -filter "Security.evtx" -forensic
+Copy-LiveResponse -path $In -dest $Out -filter "System.evtx" -forensic
+Copy-LiveResponse -path $In -dest $Out -filter "Application.evtx" -forensic
+Copy-LiveResponse -path $In -dest $Out -filter "Microsoft-Windows-Sysmon%4Operational.evtx" -forensic
+Copy-LiveResponse -path $In -dest $Out -filter "Microsoft-Windows-Powershell%4Operational.evtx" -forensic
+Copy-LiveResponse -path $In -dest $Out -filter "Microsoft-Windows-TerminalServices-RemoteConnectionManager%4Operational.evtx" -forensic
+Copy-LiveResponse -path $In -dest $Out -filter "Microsoft-Windows-TerminalServices-LocalSessionManager%4Operational.evtx" -forensic
+Copy-LiveResponse -path $In -dest $Out -filter "Microsoft-Windows-Bits-Client%4Operational.evtx" -forensic
+Copy-LiveResponse -path $In -dest $Out -filter "Microsoft-Windows-WinRM%4Operational.evtx" -forensic
